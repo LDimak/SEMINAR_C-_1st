b98bed2962c5764d6a9193c64b7e706a5797f277
dmitrii <dimakius@gmail.com>
2022-12-11 20:56:29 +0500

    added tasks 41, 43

---
+++ Task_41/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Enter qty of elements in your massive");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int[] massive = new int[n]; //объявляем создание начального массива
+for (int i=0; i<n ; i++) //заполняем массив случайными числами от -11 до 11
+    {
+        massive[i] = new Random().Next(-11,12);
+        if (massive[i]>0) count=count+1;
+    }
+Console.WriteLine($"[{string.Join(", ", massive)}]");
+Console.WriteLine(count);  
+
---
+++ Task_43/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.Clear();
+Console.WriteLine("Enter k1");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter b1");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter k2");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter b2");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+double x, y;
+
+x = (b2-b1)/(k1-k2);
+y = k1 * (b2-b1)/(k1-k2) + b1;
+Console.WriteLine($"x = {x}, y = {y}");
