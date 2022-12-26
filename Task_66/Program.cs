f8d1e8ea0c66b2f6c865d3e51986cae03eb2a3f8
dmitrii <dimakius@gmail.com>
2022-12-26 21:12:15 +0100

    Added tasks 66, 68

---
+++ Task_66/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму  элементов в промежутке от M до N. */
+
+int rec(int m, int n)
+{
+    if (m == n)
+        return 0;
+    return rec(m + 1, n) + m;
+    
+}
+Console.Clear();
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m, n));
